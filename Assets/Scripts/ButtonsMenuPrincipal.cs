df1c0f9ff1088bc471dc5de5a0751353e7f58d64
BNJ3323 <guitouguitou23@gmail.com>
2025-04-07 17:30:49 +0200

    menuprincipal
    commencement du menu principal

---
+++ Assets/Scripts/ButtonsMenuPrincipal.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonsMenuPrincipal : MonoBehaviour
+{
+    [SerializeField] private Button _buttonPlay;
+    [SerializeField] private Button _buttonSettings;
+    [SerializeField] private Button _buttonLeave;
+
+    [SerializeField] private GameObject _panelMenuPrincipal;
+    [SerializeField] private GameObject _panelSettings;
+
+
+
+    public void Leave()
+    {
+        if(_buttonLeave != null)
+        {
+            Application.Quit();
+        }
+    }
+
+    public void Settings()
+    {
+        if(_buttonSettings != null)
+        {
+            _panelMenuPrincipal.SetActive(false);
+            _panelSettings.SetActive(true);
+        }
+    }
+
+    public void Play()
+    {
+        if( _buttonPlay != null)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
