a0010e82129e6f84e6a55d3977f6c81d6960f195
takuryto2 <louisguichard761@gmail.com>
2025-04-08 17:30:24 +0200

    push du soir

--- Assets/Scripts/Room Generation/LevelGenerator.cs
+++ Assets/Scripts/Room Generation/LevelGenerator.cs
@@ -3,21 +3,35 @@ using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [Header("Global Variables")]
+    [HideInInspector] public float roomsGenerated;
+    
     [Header("Entrance Settings")]
     [SerializeField] GameObject entrancePrefab;
+    [SerializeField] private Transform[] topEntranceSpawns;
+    [SerializeField] private Transform[] leftEntranceSpawns;
+    [SerializeField] private Transform[] rightEntranceSpawns;
+    [SerializeField] private Transform[] bottomEntranceSpawns;
     
     [Header("Room Settings")]
     [SerializeField] float moveDistance;
     [SerializeField] int MoveAmount;
     [SerializeField] int minMoveAmount;
     [SerializeField] int maxMoveAmount;
-    public GameObject[] roomsArray;
+    [SerializeField] GameObject[] roomsArray;
     private int direction;
     
     
     void Start()
     {
+        switch (Random.Range(1, 5))
+        {
+            case 1:
+                
+                break;
+        }
         Instantiate(entrancePrefab, transform.position, Quaternion.identity);
+        roomsGenerated += 1;
         Move();
     }
 
@@ -25,27 +39,27 @@ public class LevelGenerator : MonoBehaviour
     {
         for (; MoveAmount > 0; MoveAmount -= Random.Range(minMoveAmount, maxMoveAmount))
         {
-            
+            if (roomsGenerated >= 256)
+            {
+                return;
+            }
             direction = Random.Range(1, 5);
             switch (direction)
             {
                 case 1:/*UP*/
-                    print("up");
                     transform.position += new Vector3(moveDistance, 0, 0);
                     break;
                 case 2:/*Left*/
-                    print("left");
                     transform.position += new Vector3(0, moveDistance, 0);
                     break;
                 case 3:/*Right*/
-                    print("right");
                     transform.position -= new Vector3(moveDistance, 0, 0);
                     break;
                 case 4:/*Down*/
-                    print("down");
                     transform.position -= new Vector3(0, moveDistance, 0);
                     break;
             }
+            roomsGenerated += 1;
             Instantiate(roomsArray[0], transform.position, Quaternion.identity);
         }
     }
