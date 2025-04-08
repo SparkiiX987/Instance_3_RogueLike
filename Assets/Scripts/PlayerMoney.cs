f4abf4a79ff4f236df9edd3ba4697a2b3215cb8c
BNJ3323 <guitouguitou23@gmail.com>
2025-04-08 17:31:16 +0200

    Liaison UI et shop

---
+++ Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    private int _money;
+
+    public int GetMoney()
+    {
+        return _money;
+    }
+
+    public void AddMoney(int amount)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public static Shop Instance;
+
+    public ShopOffer activeOffer;
+    public ShopOffer[] offers = new ShopOffer[5];
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.Log("trop de fois appelé");
+        }
+        Instance = this;
+    }
+
+    public void SetActiveOffer(ShopOffer activeOffers)
+    {
+
+    }
+
+    public void ActualiseOffers()
+    {
+
+    }
+}
