f06f108600ae15af03c7b04446af69196ac991f4
takuryto2 <louisguichard761@gmail.com>
2025-04-09 13:57:34 +0200

    spawn de l'entrée + début clamp les déplacments

--- Assets/Scripts/Room Generation/LevelGenerator.cs
+++ Assets/Scripts/Room Generation/LevelGenerator.cs
@@ -1,41 +1,77 @@
+using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
+    #region Global Variables
+    
     [Header("Global Variables")]
     [HideInInspector] public float roomsGenerated;
-    
+
+    #endregion
+
+    #region Entrance Settings
+
     [Header("Entrance Settings")]
-    [SerializeField] GameObject entrancePrefab;
+    [SerializeField] GameObject[] entrancePrefabs;
     [SerializeField] private Transform[] topEntranceSpawns;
     [SerializeField] private Transform[] leftEntranceSpawns;
     [SerializeField] private Transform[] rightEntranceSpawns;
     [SerializeField] private Transform[] bottomEntranceSpawns;
+
+    #endregion
+
+    #region Room Settings
     
     [Header("Room Settings")]
-    [SerializeField] float moveDistance;
+    [SerializeField] float maxPosX;
+    [SerializeField] float maxPosY;
     [SerializeField] int MoveAmount;
     [SerializeField] int minMoveAmount;
     [SerializeField] int maxMoveAmount;
+    [SerializeField] float moveDistance;
     [SerializeField] GameObject[] roomsArray;
     private int direction;
     
+    #endregion
     
     void Start()
     {
-        switch (Random.Range(1, 5))
-        {
-            case 1:
-                
-                break;
-        }
-        Instantiate(entrancePrefab, transform.position, Quaternion.identity);
-        roomsGenerated += 1;
-        Move();
+        SetEntranceSpawn();
+        WalkerMovment();
     }
 
-    private void Move()
+    private void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, 0, maxPosX),
+            Mathf.Clamp(transform.position.y, 0, maxPosY), transform.position.z);
+    }
+
+    private void SetEntranceSpawn()
+    {
+        int selectedPrefab = Random.Range(0, 4);
+        switch (selectedPrefab)
+        {
+            case 0:/*UP*/
+                transform.position = topEntranceSpawns[Random.Range(0, topEntranceSpawns.Length)].position;
+                break;
+            case 1:/*Left*/
+                transform.position = leftEntranceSpawns[Random.Range(0, leftEntranceSpawns.Length)].position;
+                break;
+            case 2:/*Right*/
+                transform.position = rightEntranceSpawns[Random.Range(0, rightEntranceSpawns.Length)].position;
+                break;
+            case 3:/*Down*/
+                transform.position = bottomEntranceSpawns[Random.Range(0, bottomEntranceSpawns.Length)].position;
+                break;
+        }
+        Instantiate(entrancePrefabs[selectedPrefab], transform.position, Quaternion.identity);
+        roomsGenerated += 1;
+    }
+
+    private void WalkerMovment()
     {
         for (; MoveAmount > 0; MoveAmount -= Random.Range(minMoveAmount, maxMoveAmount))
         {
@@ -43,24 +79,56 @@ public class LevelGenerator : MonoBehaviour
             {
                 return;
             }
-            direction = Random.Range(1, 5);
+            direction = Random.Range(0, 4);
             switch (direction)
             {
-                case 1:/*UP*/
-                    transform.position += new Vector3(moveDistance, 0, 0);
+                case 0:/*UP*/
+                    print("up " + transform.position);
+                    if (transform.position.y < maxPosY) {
+                        transform.position += new Vector3(moveDistance, 0, 0);
+                        roomsGenerated += 1;
+                        RoomGenerator();
+                    }
                     break;
-                case 2:/*Left*/
-                    transform.position += new Vector3(0, moveDistance, 0);
+                case 1:/*Left*/
+                    print("left " + transform.position);
+                    if (transform.position.x > 0) {
+                        transform.position += new Vector3(0, moveDistance, 0);
+                        roomsGenerated += 1;
+                        RoomGenerator();
+                    }
                     break;
-                case 3:/*Right*/
-                    transform.position -= new Vector3(moveDistance, 0, 0);
+                case 2:/*Right*/
+                    print("right " + transform.position);
+                    if (transform.position.x < maxPosX) {
+                        transform.position -= new Vector3(moveDistance, 0, 0);
+                        roomsGenerated += 1;
+                        RoomGenerator();
+                    }
                     break;
-                case 4:/*Down*/
-                    transform.position -= new Vector3(0, moveDistance, 0);
+                case 3:/*Down*/
+                    print("down " + transform.position);
+                    if (transform.position.y > 0) {
+                        transform.position -= new Vector3(0, moveDistance, 0);
+                        roomsGenerated += 1;
+                        RoomGenerator();
+                    }
                     break;
             }
-            roomsGenerated += 1;
-            Instantiate(roomsArray[0], transform.position, Quaternion.identity);
         }
+    }
+
+    private void RoomGenerator()
+    {
+        Instantiate(roomsArray[0], transform.position, Quaternion.identity);
+    }
+
+    private bool IsInbounds()
+    {
+        if (transform.position.x > maxPosX && transform.position.x < maxPosX)
+        {
+            
+        }
+        return false;
     }
 }
