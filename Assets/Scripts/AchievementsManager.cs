581be6e00183b15cafca5d3d9556f61986a01a61
lukabartlo <l.bartolo@lahorde.school>
2025-04-18 11:54:27 +0200

    Push Achiements
    Fait en sorte que l'animation se spam pas

--- Assets/Scripts/AchievementsManager.cs
+++ Assets/Scripts/AchievementsManager.cs
@@ -8,34 +8,33 @@ using UnityEngine.UI;
 public class AchievementsManager : MonoBehaviour
 {
     [SerializeField] private GameObject achievementPanel;
+    [SerializeField] private Image achievementImage;
+    [SerializeField] private TextMeshProUGUI achievementTitle;
+    [SerializeField] private TextMeshProUGUI achievementDesc;
     
     public List<Sprite> achievementsIcons = new List<Sprite>();
     public List<String> achievementsTitles = new List<String>();
     public List<String> achievementsDescs = new List<String>();
     
-    private Image achievementImage;
-    private TextMeshProUGUI achievementTitle;
-    private TextMeshProUGUI achievementDesc;
-    
-    private void Start()
-    {
-        achievementImage = achievementPanel.GetComponentInChildren<Image>();
-        achievementTitle = achievementPanel.GetComponentInChildren<TextMeshProUGUI>();
-        achievementDesc = achievementPanel.GetComponentInChildren<TextMeshProUGUI>();
-    }
+    private bool isPlaying = false;
 
-    private async void AddAchievements(int achievementsID)
+    private async Awaitable AddAchievements(int achievementsID)
     {
-        PlayerPrefs.SetInt(achievementsID.ToString(), 1);
-        achievementPanel.SetActive(true);
-        achievementTitle.text = achievementsTitles[achievementsID];
-        achievementDesc.text = achievementsDescs[achievementsID];
-        achievementImage.sprite = achievementsIcons[achievementsID];
-        achievementPanel.transform.DOMoveY(990, 2f);
-        await Awaitable.WaitForSecondsAsync(5f);
-        achievementPanel.transform.DOMoveY(1230, 2f);
-        await Awaitable.WaitForSecondsAsync(3f);
-        achievementPanel.SetActive(false);
+        if (!isPlaying)
+        {
+            PlayerPrefs.SetInt(achievementsID.ToString(), 1);
+            achievementPanel.SetActive(true);
+            isPlaying = true;
+            achievementTitle.text = achievementsTitles[achievementsID];
+            achievementDesc.text = achievementsDescs[achievementsID];
+            achievementImage.sprite = achievementsIcons[achievementsID];
+            achievementPanel.transform.DOMoveY(990, 2f);
+            await Awaitable.WaitForSecondsAsync(5f);
+            achievementPanel.transform.DOMoveY(1230, 2f);
+            await Awaitable.WaitForSecondsAsync(3f);
+            achievementPanel.SetActive(false);
+            isPlaying = false;
+        }
     }
 
     public void PlayAddAchievement()
