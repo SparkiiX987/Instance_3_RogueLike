77cbc8d58738541c0b73ca8a278366cc74f4c144
SparkiiX987 <aure.dugue@gmail.com>
2025-04-18 11:02:46 +0200

    rremove caca

--- Assets/Scripts/EmptyBottle.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class EmptyBottle : UsableObject
-{
-    public override void Action()
-    {
-        AudioManager.Instance.PlaySound(AudioType.Throw);
-    }
-}
--- Assets/Scripts/PepperSpray.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PepperSpray : UsableObject
-{
-    public override void Action()
-    {
-        AudioManager.Instance.PlaySound(AudioType.lacrymogene);
-    }
-}
