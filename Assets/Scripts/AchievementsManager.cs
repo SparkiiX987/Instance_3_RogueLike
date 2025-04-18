5022851597d2051cf9e1efcedfb1ba908dbd9f65
lukabartlo <l.bartolo@lahorde.school>
2025-04-18 12:02:25 +0200

    Push Achievements
    Fait en sorte que plusieurs achievement se joeu a la suite

--- Assets/Scripts/AchievementsManager.cs
+++ Assets/Scripts/AchievementsManager.cs
@@ -16,29 +16,36 @@ public class AchievementsManager : MonoBehaviour
     public List<String> achievementsTitles = new List<String>();
     public List<String> achievementsDescs = new List<String>();
     
+    public List<int> compAchievementList = new List<int>();
+    
     private bool isPlaying = false;
 
     private async Awaitable AddAchievements(int achievementsID)
     {
-        if (!isPlaying)
+        compAchievementList.Add(achievementsID);
+        
+        while (compAchievementList.Count > 0 && !isPlaying)
         {
             PlayerPrefs.SetInt(achievementsID.ToString(), 1);
             achievementPanel.SetActive(true);
             isPlaying = true;
-            achievementTitle.text = achievementsTitles[achievementsID];
-            achievementDesc.text = achievementsDescs[achievementsID];
-            achievementImage.sprite = achievementsIcons[achievementsID];
+            achievementTitle.text = achievementsTitles[compAchievementList[achievementsID]];
+            achievementDesc.text = achievementsDescs[compAchievementList[achievementsID]];
+            achievementImage.sprite = achievementsIcons[compAchievementList[achievementsID]];
             achievementPanel.transform.DOMoveY(990, 2f);
             await Awaitable.WaitForSecondsAsync(5f);
             achievementPanel.transform.DOMoveY(1230, 2f);
             await Awaitable.WaitForSecondsAsync(3f);
             achievementPanel.SetActive(false);
             isPlaying = false;
+            compAchievementList.Remove(compAchievementList[achievementsID]);
         }
     }
 
-    public void PlayAddAchievement()
+    public async void PlayAddAchievement()
     {
-        AddAchievements(1);
+        AddAchievements(0);
+        await Awaitable.WaitForSecondsAsync(2f);
+        AddAchievements(8);
     }
 }
