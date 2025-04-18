f4cbf2f0f9a39482376d00750f75736635064170
SparkiiX987 <aure.dugue@gmail.com>
2025-04-18 17:36:32 +0200

    Update MonsterCan.cs

--- Assets/Scripts/Items/MonsterCan.cs
+++ Assets/Scripts/Items/MonsterCan.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class MonsterCan : UsableObject
 {
     public float bonusDuration;
-    private float elapsedTime;
     private PlayerControl playerControl;
 
     public MonsterCan(int _price, string _name, string _description, int _type) : base(_price, _name, _description, _type)
